269ad0575ac0d5a51428e119bd97287589181ab0
SOURAV MANDAL <sourav0200@gmail.com>
2023-05-02 22:34:51 +0600

    Task 1 problem 1

---
+++ Final Term Tasks/Lab Task-1/Task1.asm
@@ -0,0 +1,32 @@
+.model small
+.stack 100h
+.data
+.code
+
+main proc 
+   
+    mov al, 'a'   
+    cmp al, 'b'
+    jle then     
+    mov cl, '5'
+    jmp print_data 
+    jmp exit
+
+    
+then:
+   
+    mov cl, '7'
+    jmp print_data
+    jmp exit  
+    
+print_data:   
+  
+    mov ah, 02h 
+    add dl, cl
+    int 21h     
+    
+exit:
+    mov ah,4ch 
+    int 21h
+    main endp
+end main
