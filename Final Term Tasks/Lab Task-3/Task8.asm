eae8c6a89acfb133a9d32f772041f703c539fd74
SOURAV MANDAL <sourav0200@gmail.com>
2023-05-03 00:00:52 +0600

    Task 8 done, Lab 3 done

---
+++ Final Term Tasks/Lab Task-3/Task8.asm
@@ -0,0 +1,20 @@
+.model small
+.stack 100h
+.data
+.code
+main proc
+
+    mov dx, 5
+
+    mov ah , 1h
+    int 21h 
+    
+    and al, 0fh
+    mov cl, al
+    shr dx, cl
+    mov ah, 2h
+    int 21h
+    mov ah, 4ch
+    int 21h    
+    main endp
+end main
